a26b010545f31024d7ac6de9f5571132797cd86f
yaseenali512 <yaseenali.bscssef20@iba-suk.edu.pk>
2023-11-27 14:16:24 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    //if an error bubbled up the whole app will be unmounted, so we also use it in the child routes
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
